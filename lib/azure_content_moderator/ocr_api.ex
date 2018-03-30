c8a1de2c9a67cdaeb3b51f31792ad84b620b6eb4
Edgar Pino <edgarbeltran@me.com>
2018-03-30 00:36:21 -0600

    create OCRAPI client

---
+++ lib/azure_content_moderator/ocr_api.ex
@@ -0,0 +1,14 @@
+defmodule AzureCM.OCRAPI do
+  @moduledoc false
+
+  use HTTPoison.Base
+
+  alias AzureCM.Config
+
+  def process_url(url), do: Config.base_api_url() <> url
+
+  def process_response_body(body) do
+    body
+    |> JSON.decode()
+  end
+end
